3c4194f4972a327e441ce701bc090c9ac8044eca
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-29 23:50:28 +0530

    add copy right

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-      <div className="flex h-screen max-h-screen">
-        <section className="remove-scrollbar container my-auto">
-          <div className="sub-container max-w-[496px]">
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -14,8 +16,19 @@ export default function Home() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-sm mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Patient</p>
           </div>
-        </section>
-      </div>
+
+          <Link href={'/?admin=true'} >
+
+          </Link>
+
+        </div>
+      </section>
+    </div>
   );
 }
