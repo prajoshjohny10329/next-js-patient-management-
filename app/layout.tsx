e53ffbcd946941864a5877484a8f7421cb719c22
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-29 22:47:36 +0530

    add dynamic own css by fontSans.variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from '@/lib/utils'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ cn ("min-h-screen bg-dark-300 font-sans antialiased")}> {children} </body>
+      <body className={ cn ("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}> {children} </body>
     </html>
   );
 }
