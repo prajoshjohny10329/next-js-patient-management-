a4442217e1fa342265aa130fca4ee70f61529192
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-29 23:02:54 +0530

    config shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline uppercase text-center ">home</h1>
+      <h1 className="text-3xl underline uppercase text-center text-white">home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
--- log.md
+++ log.md
@@ -6,4 +6,11 @@
         * import { cn } from '@/lib/utils'
         * create lib/utils.ts
         * passing in body tag
-        
+        * remove localFont from default then fontSans.variable --app/layout.ts
+
+    configure Shadcn
+        *npx shadcn@latest init
+        *for dark mode
+
+
+
