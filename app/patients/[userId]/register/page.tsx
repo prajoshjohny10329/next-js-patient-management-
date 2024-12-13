f31691807fb6909222e4e3d89941e65931e130b1
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-12-14 00:11:50 +0530

    create design befor creating register from

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Registration = () => {
   return (
     <div className="flex h-screen max-h-screen">
-      <div className="remove-scrollbar container">
-        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -13,8 +14,25 @@ const Registration = () => {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
+
+          <div className="text-sm mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Patient
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
     </div>
   );
 };
